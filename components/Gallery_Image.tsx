03561d5f02d17463b4dddcfbc5bf0aa7c9554033
Anshul <anshulmendiratta10d@gmail.com>
2024-12-23 15:53:44 +0530

    added lightbox

--- components/Gallery_Image.tsx
+++ components/Gallery_Image.tsx
@@ -3,14 +3,20 @@ import Image from "next/image";
 interface ImageProps {
     src: string;
     alt: string;
+    onClick: (index: number) => void;
     className?: string;
+    index: number;
 }
 
-const GalleryImage: React.FC<ImageProps> = ({src, alt, className}) => {
+const GalleryImage: React.FC<ImageProps> = ({src, alt, onClick, className, index}) => {
+    const handleClick = () => {
+        onClick(index);
+    };
+
     return (
-        <div className="group">
-            <div className="overflow-hidden rounded-lg relative aspect-w-1 aspect-h-1">
-                <Image height={200} width={380} alt={alt} src={src} className={`transition ease duration-1000 group-hover:scale-110 ${className}`} objectFit="cover" />
+        <div>
+            <div className="group overflow-hidden rounded-lg relative aspect-w-1 aspect-h-1" onClick={handleClick}>
+                    <Image height={200} width={380} alt={alt} src={src} className={`transition ease duration-1000 group-hover:scale-110 ${className}`} objectFit="cover" />
             </div>
         </div>
     );
