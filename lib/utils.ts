2ccfc1553a42190d8e8c231e9299d1638848cb0e
pihu26112005 <piyushkumar26november@gmail.com>
2025-01-08 03:07:28 +0530

    Anouncement Page Completed

--- lib/utils.ts
+++ lib/utils.ts
@@ -495,3 +495,66 @@ export const teamsList = [
     ],
   },
 ];
+
+
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+export const faqquestions = [
+  {
+    id: 1,
+    question: "Popular Articles",
+    answer: "Details about popular articles.",
+  },
+  {
+    id: 2,
+    question: "Fix problems & request removals",
+    answer: "Details about fixing problems and removals.",
+  },
+  {
+    id: 3,
+    question: "Browse the web",
+    answer: "Details about browsing the web.",
+  },
+  {
+    id: 4,
+    question: "Search on your phone or tablet",
+    answer: "Details about searching on your device.",
+  },
+];
+
+
+export const announcementFaq = [
+  {
+    quote:
+      "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair.",
+    name: "Charles Dickens",
+    title: "A Tale of Two Cities",
+  },
+  {
+    quote:
+      "To be, or not to be, that is the question: Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune, Or to take Arms against a Sea of troubles, And by opposing end them: to die, to sleep.",
+    name: "William Shakespeare",
+    title: "Hamlet",
+  },
+  {
+    quote: "All that we see or seem is but a dream within a dream.",
+    name: "Edgar Allan Poe",
+    title: "A Dream Within a Dream",
+  },
+  {
+    quote:
+      "It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife.",
+    name: "Jane Austen",
+    title: "Pride and Prejudice",
+  },
+  {
+    quote:
+      "Call me Ishmael. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world.",
+    name: "Herman Melville",
+    title: "Moby-Dick",
+  },
+];
+
+
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
