68167cf730125f0f5f5d27bb1917f1639a44506e
Player <141478611+PLAYER1267@users.noreply.github.com>
2025-01-07 20:24:46 +0530

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.(mp4|webm|ogg)$/i,
+      loader: 'file-loader',
+      options: {
+        name: 'static/media/[name].[hash].[ext]', // Customize the filename pattern
+      },
+    });
 
+    return config;
+  },
+};
 
 export default nextConfig;
