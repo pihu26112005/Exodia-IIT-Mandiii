6a9ba032a74579d0b89cd8cbc64f8b4f99b50ff3
pihu26112005 <piyushkumar26november@gmail.com>
2025-01-08 15:39:21 +0530

    Desktop Navbar fully updated

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar: React.FC = () => {
 
   const [dropdownOpen, setDropdownOpen] = useState(null);
 
-  const toggleDropdown = (index:any) => {
+  const toggleDropdown = (index: any) => {
     setDropdownOpen(dropdownOpen === index ? null : index);
   };
 
@@ -57,45 +57,45 @@ const Navbar: React.FC = () => {
           Exodia
         </a>
 
-         {/* Navbar Links */}
-      <nav className="hidden sm:flex flex-row items-center space-x-6">
-        {navItems.map((item, index) => (
-          <div key={item.name} 
-          className="relative group p-2"
-          onMouseEnter={() => item.subItems && toggleDropdown(index)}
-          onMouseLeave={() => item.subItems && toggleDropdown(null)}
-          >
-            <div className="flex">
-            <a
-              href={item.link}
-              className="text-base hover:text-yellow-300 transition"
-              
+        {/* Navbar Links */}
+        <nav className="hidden sm:flex flex-row items-center space-x-6">
+          {navItems.map((item, index) => (
+            <div key={item.name}
+              className="relative group p-2"
+              onMouseEnter={() => item.subItems && toggleDropdown(index)}
+              onMouseLeave={() => item.subItems && toggleDropdown(null)}
             >
-              {item.name}
-            </a>
-            {item.subItems && (
-              <Image src="/icons/custom/dropdown.svg" alt="Arrow Down" width={20} height={20} style={{ filter: 'invert(1)' }} />
-            )}
-            </div>
-            {/* Dropdown Menu */}
-            {item.subItems && dropdownOpen === index && (
-              <div className="absolute top-full left-0 bg-gray-800 text-white  rounded-2xl shadow-lg  bg-white/10 backdrop-blur-md border-b border-white/20 transition-transform duration-300">
-                {item.subItems.map((subItem) => (
-                  <a
-                    key={subItem.name}
-                    href={subItem.link}
-                    className="block px-4 py-2 hover:text-yellow-300 transition"
-                  >
-                    {subItem.name}
-                  </a>
-                ))}
-              </div>
-            )}
-          </div>
-        ))}
-      </nav>
+              <div className="flex">
+                <a
+                  href={item.link}
+                  className="text-base hover:text-yellow-300 transition"
 
-        
+                >
+                  {item.name}
+                </a>
+                {item.subItems && (
+                  <Image src="/icons/custom/dropdown.svg" alt="Arrow Down" width={20} height={20} style={{ filter: 'invert(1)' }} />
+                )}
+              </div>
+              {/* Dropdown Menu */}
+              {item.subItems && dropdownOpen === index && (
+                <div className="border-2 border-white dark:border-white/[0.2] absolute top-full left-0 bg-gray-800 text-white  rounded-2xl shadow-lg  bg-white/10 backdrop-blur-md border-b border-white/20 transition-transform duration-300">
+                  {item.subItems.map((subItem) => (
+                    <a
+                      key={subItem.name}
+                      href={subItem.link}
+                      className="block px-4 py-2 hover:text-yellow-300 transition"
+                    >
+                      {subItem.name}
+                    </a>
+                  ))}
+                </div>
+              )}
+            </div>
+          ))}
+        </nav>
+
+
         {/* Menu */}
         <div className="max-sm:hidden">
           P
