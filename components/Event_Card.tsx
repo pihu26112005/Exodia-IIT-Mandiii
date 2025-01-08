4aa4d7bb0590450e72b8d3c65b1984f983dc2fa4
pihu26112005 <piyushkumar26november@gmail.com>
2025-01-08 15:52:02 +0530

    connected events in event page to event detail page

--- components/Event_Card.tsx
+++ components/Event_Card.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './ui/Card.css';  // Import your CSS file
+import Link from 'next/link';
 
 interface CardProps {
     imageUrl: string;   // Image URL for the background image
@@ -11,22 +12,25 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({ imageUrl, title, description, location, price }) => {
     return (
-        <div className='event-card'>
-        <div className="card border-2 border-yellow-300 rounded-3xl">
-            <div className="card-img" style={{ backgroundImage: `url(${imageUrl})` }}></div>
-            <div className="card-body">
-                <span className="bg"></span>
-                <span className="bg"></span>
-                <span className="bg"></span>
-                <div className="content flex flex-col justify-center items-center">
-                    <h2 className="title">{title}</h2>  {/* Title */}
-                    <p className="short-description">{description}</p>  {/* Short description */}
-                    <p className="location">Loc : {location}</p>  {/* Location */}
-                    <p className="price">price : {price}</p>  {/* Price */}
-                    <button className="register-button">Register Now</button>  {/* Register button */}
+       <Link href={`/eventDetail/${title}`}>
+         <div className='event-card'>
+            <div className="card border-2 border-yellow-300 rounded-3xl">
+                <div className="card-img" style={{ backgroundImage: `url(${imageUrl})` }}></div>
+                <div className="card-body">
+                    <span className="bg"></span>
+                    <span className="bg"></span>
+                    <span className="bg"></span>
+                    <div className="content flex flex-col justify-center items-center">
+                        <h2 className="title">{title}</h2>  {/* Title */}
+                        <p className="short-description">{description}</p>  {/* Short description */}
+                        <p className="location">Loc : {location}</p>  {/* Location */}
+                        <p className="price">price : {price}</p>  {/* Price */}
+                        <button className="register-button">Register Now</button>  {/* Register button */}
+                    </div>
                 </div>
             </div>
-        </div></div>
+        </div>
+        </Link>
     );
 };
 
