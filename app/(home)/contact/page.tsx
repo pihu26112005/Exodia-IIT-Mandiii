d998bf564fb9c558243da659604709fb2291180b
pihu26112005 <piyushkumar26november@gmail.com>
2025-01-10 18:33:46 +0530

    Contact Page Improved

--- app/(home)/contact/page.tsx
+++ app/(home)/contact/page.tsx
@@ -9,19 +9,23 @@ import Contact_Card from '@/components/Contact_Card'
 const page: React.FC = () => {
   return (
     <>
-      <Navbar/>
+      <Navbar />
 
-      <div className=' h-[100vh] flex justify-center items-center flex-wrap'>
-        <Contact_Card src="/assets/contact/c1.jpeg" title='Albus Dumbledore' desc="Headmaster" contact="+91-987384738" />
+      <div className='p-0 m-0 h-full'>
+        <div className='flex justify-center items-center flex-wrap'>
+          <div className='mt-[10rem] flex justify-center items-center flex-wrap'>
+            <Contact_Card src="/assets/contact/c1.jpeg" title='Albus Dumbledore' desc="Headmaster" contact="+91-987384738" />
 
-        <Contact_Card src="/assets/contact/c2.jpeg" title="Minerva Mcgonagall" desc="Professor of Transfiguration" contact="+91-987384738" />
+            <Contact_Card src="/assets/contact/c2.jpeg" title="Minerva Mcgonagall" desc="Professor" contact="+91-987384738" />
 
-        <Contact_Card src="/assets/contact/c1.jpeg" title="Severus Snape" desc="Professor of Magical Posions" contact="+91-987384738" />
+            <Contact_Card src="/assets/contact/c1.jpeg" title="Severus Snape" desc="Professor" contact="+91-987384738" />
 
-        <Contact_Card src="/assets/contact/c2.jpeg" title="Harry Potter" desc="Head boy" contact="+91-987384738" />
+            <Contact_Card src="/assets/contact/c2.jpeg" title="Harry Potter" desc="Head boy" contact="+91-987384738" />
+          </div>
+        </div>
       </div>
 
-      <Footer/>
+      <Footer />
     </>
   )
 }
