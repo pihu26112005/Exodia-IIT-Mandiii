75c0b82e141896cf6203c1122d165c0c067eadf7
pihu26112005 <piyushkumar26november@gmail.com>
2025-01-25 00:40:00 +0530

    explore more button completed

--- components/HomeEvents.tsx
+++ components/HomeEvents.tsx
@@ -7,15 +7,21 @@ export function HomeEvents() {
   return (
     <div className="box12 h-[100vh] flex justify-center items-center">
       {/* <BackgroundGradientAnimation> */}
-        <div className="z-50 ">
-          <div id="events" className="relative z-50  flex flex-col justify-center items-center gap-4">
-            <Home_EventsSection />
-            <br />
-            <Link href="/events">
+      <div className="z-50 ">
+        <div id="events" className="relative z-50  flex flex-col justify-center items-center gap-4">
+          <Home_EventsSection />
+          <br />
+          {/* <Link href="/events">
               <ArrowButton text='more events' />
-            </Link><br /><br /><br /><br /><br />
+            </Link><br /><br /><br /><br /><br /> */}
+          <div className="mb-7 mt-5">
+          <Link href="/3d" className='p-4 rounded-3xl border-2 border-black bg-gold text-black text-[1.5rem] animate-buttonshine'>
+          More Events
+          </Link>
           </div>
+          <br /><br /><br /><br /><br /> 
         </div>
+      </div>
       {/* </BackgroundGradientAnimation> */}
     </div>
   );
