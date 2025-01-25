667f53239b0b6dbca6a1ac10f38560283d45dc0f
pihu26112005 <piyushkumar26november@gmail.com>
2025-01-25 21:31:19 +0530

    newspaper update

--- components/Anouncement_NewsSection.tsx
+++ components/Anouncement_NewsSection.tsx
@@ -32,16 +32,20 @@ const Anouncement_NewsSection = () => {
             <div className={`bg-repeat bg-center  w-full text-black`}
                 style={{ backgroundImage: "url('/assets/anouncement/nbg.avif')" }}>
                 <div className='flex flex-col justify-center items-center'>
-                    <div className={`pt-[6rem] text-[70px] ${aboutNPfont.className}`}>
-                        The Exodia Times
+                    <div className={`pt-[6rem] max-sm:pt-[4rem] text-[70px] ${aboutNPfont.className}`}>
+                        The Exodia Prophet
                     </div>
 
                     <div className='flex justify-center items-center'>
                         <hr className="absolute w-[80%] h-[2px] bg-black mb-1 " />
                         <hr className="absolute w-[80%] h-[1px] bg-black mt-1 " />
                     </div>
-                    <div className={`mt-2 mb-2 w-[80%] text-[20px] ${aboutNPfont2.className}`}>
-                        Announcement
+                    <div className={`flex items-center justify-between max-sm:justify-center mt-2 mb-2 w-[80%] text-[20px] ${aboutNPfont2.className}`}>
+                        <div className='max-sm:hidden'></div>
+                        <div className='max-sm:hidden'>exodia</div>
+                        <div>Announcement</div>
+                        <div className='max-sm:hidden'>11-15 March</div>
+                        <div className='max-sm:hidden'></div>
                     </div>
                     <div className='flex justify-center items-center'>
                         <hr className="absolute w-[80%] h-[1px] bg-black mt-1 " />
@@ -49,7 +53,7 @@ const Anouncement_NewsSection = () => {
                     </div>
 
                     {/* Map through the announcements array */}
-                    <div className='flex flex-col justify-center items-center w-[80%]  max-sm:w-[90%] mt-5'>
+                    <div className='flex flex-col justify-center items-center w-[80%]  max-sm:w-[90%] mt-7 max-sm:mt-8'>
                         {announcements.map((item, index) => (
                             <>
                                 <div key={index} className='flex flex-row justify-between items-center w-full'>
@@ -59,8 +63,8 @@ const Anouncement_NewsSection = () => {
                                         </div>
                                         <div className='flex max-sm:flex-col w-[80%] mt-4 mb-8'>
                                             {item.image && (
-                                                <div className='w-[40%] flex justify-start items-center'>
-                                                    <Image className='w-[20rem]' src={item.image} alt={item.title} width={200} height={200} />
+                                                <div className='w-[40%] flex justify-start items-center '>
+                                                    <Image className='w-[20rem] border border-black transition-all duration-500 hover:grayscale-0 grayscale' src={item.image} alt={item.title} width={200} height={200} />
                                                 </div>
                                             )}
                                             <div className={`${item.image ? "w-[30%] max-sm:w-[100%] mt-2" : "w-[80%]"} text-[15px] max-sm:text-[10px] ${aboutNPfont4.className} text-start `}>
@@ -69,7 +73,7 @@ const Anouncement_NewsSection = () => {
                                         </div>
                                     </div>
                                     <div className='w-[1px] h-full '>
-                                        <hr className="w-[1px] h-[100px] bg-black" />
+                                        <hr className="w-[1px] h-[100px] max-sm:h-[150px] bg-black" />
                                     </div>
                                     <div className='w-[25%] max-sm:w-[15%] right-0 flex flex-col justify-center items-center'>
                                         <div className={`${aboutNPfont4.className} max-sm:text-[10px]`}>
